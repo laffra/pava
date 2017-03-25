6f717384920a0a883e51aecdf420e778c041c45b
laffra@gmail.com <iV29VQzQVT11>
2017-03-25 19:14:58 -0400

    restructure file locations

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from pava.implementation import classloader
-
-set_classpath = classloader.set_classpath
-method = implementation.method
-nan = implementation.nan
-inf = implementation.inf
--- demo.py
+++ demo.py
@@ -2,18 +2,19 @@ import pava
 
 
 # Tell pava where it can find Java user-defined classes
-#pava.set_classpath(['c:/Users/laffr/PycharmProjects/pava/pava'])
+pava.set_classpath(['c:/Users/laffr/PycharmProjects/pava/pava'])
 
-import java
-class Out(object):
-    def println(self, s):
-        print s
-java.lang.System.out = Out()
+try:
+    import java
+    class Out(object):
+        def println(self, s):
+            print s
+    java.lang.System.out = Out()
+except ImportError:
+    pass
 
 
 # Load a Java class and call a static method on it from Python
 
-import examples
-
-examples.HelloWorld.main()
+HelloWorld.main()
 
