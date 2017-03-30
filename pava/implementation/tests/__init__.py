c1f7544138990dc6dbb05090711d57e6fea36fb4
laffra@gmail.com <iV29VQzQVT11>
2017-03-30 11:46:06 -0400

    Create all.py to run all tests

--- pava/implementation/tests/__init__.py
+++ pava/implementation/tests/__init__.py
@@ -1,12 +1,1 @@
-import unittest
 
-import arrays
-
-ArrayTest = arrays.ArrayTest
-
-class PavaTest(unittest.TestCase):
-    def test_general(self):
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
